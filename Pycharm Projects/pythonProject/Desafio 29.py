cce762731b09f6a950993e852436a56d07eb7ea2
amarall06 <amarall06@users.noreply.github.com>
2024-10-08 19:39:46 -0300

    a

---
+++ Pycharm Projects/pythonProject/Desafio 29.py
@@ -0,0 +1,7 @@
+vel = int(input('Digite a velocidade que voce estava:'))
+m =
+if vel > 80:
+    print('Voce passou do limite da via, voce foi MULTADO!')
+    print('A sua multa vai custar R$7,00 por cada km, ou seja, voce vai pagar {:.2f} de multa'.format(m))
+else:
+    print('')
--- Pycharm Projects/pythonProject/aula10.py
+++ Pycharm Projects/pythonProject/aula10.py
@@ -1,6 +1,8 @@
-nome = int(input('Digite um numero para saber se é par ou impar:'))
-resto = nome % 2
-if resto == 0:
-    print('O Número é par!')
+n1 = float(input('digite a primeira nota:'))
+n2 = float(input('digite a segunda nota:'))
+m = (n1 + n2) /2
+print('A sua média foi {:.1f}'.format(m))
+if m >= 6:
+    print('Sua média foi boa, PARABÉNS!')
 else:
-    print('O Número é impar!')
+    print('Sua média foi ruim, ESTUDE MAIS!')
