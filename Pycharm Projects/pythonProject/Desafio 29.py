6d15599fb5d64c9f5e86f1cef39700812aee2bdd
amarall06 <amarall06@users.noreply.github.com>
2024-10-08 21:40:02 -0300

    a

--- Pycharm Projects/pythonProject/Desafio 29.py
+++ Pycharm Projects/pythonProject/Desafio 29.py
@@ -1,7 +1,7 @@
 vel = int(input('Digite a velocidade que voce estava:'))
-m =
 if vel > 80:
     print('Voce passou do limite da via, voce foi MULTADO!')
-    print('A sua multa vai custar R$7,00 por cada km, ou seja, voce vai pagar {:.2f} de multa'.format(m))
-else:
-    print('')
+    multa = (vel - 80) * 7
+    print('A sua multa vai custar R$7,00 por cada km, ou seja, voce vai pagar {:.2f} de multa'.format(multa))
+print('Tenha um Bom dia! Dirija com segurança!')
+
