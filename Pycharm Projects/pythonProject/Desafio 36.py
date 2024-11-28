6de47576c9b9d945431fd80de448978f06efe06f
amarall06 <amarall06@users.noreply.github.com>
2024-11-28 15:11:14 -0300

    python desafios

--- Pycharm Projects/pythonProject/Desafio 36.py
+++ Pycharm Projects/pythonProject/Desafio 36.py
@@ -1,8 +1,13 @@
 imovel = float(input('Qual é o valor do imovel?'))
 salario = float(input('Salário do Comprador: R$'))
 anos = int(input('Quantos anos de financimento?'))
-min = 
-parc = imovel / (anos * 12)
+min = salario * 30 / 100
+parc = imovel / (anos * 12) # porcentagem
 print('Para pagar a casa de R${:.2f} em {} anos'.format(imovel, anos), end='')
 print('A prestação será de {:.2f}'.format(parc))
+if parc <= min:
+    print('O Emprestimo foi ACEITO!')
+else:
+    print('O Emprestimo foi NEGADO!')
+
 
--- Pycharm Projects/pythonProject/Desafio 39.py
+++ Pycharm Projects/pythonProject/Desafio 39.py
@@ -1,9 +1,16 @@
-idade = int(input('Em que ano voce nasceu?'))
-res = idade
-if idade < 2006:
-    print('Ja passou da hora de se alistar!!')
-elif idade == 2006:
-    print('Ja esta na idade de servir ao exercito!')
-else:
-    tempo = idade // 18
-    print('Você ainda precisa se alistar ao serviço militar. faltam {} anos'.format(tempo))
+from datetime import date
+atual = date.today().year
+nasc = int(input('Em que ano voce nasceu?'))
+idade = atual - nasc
+print('Quem nasceu em {} tem {} anos  em {}'.format(nasc, idade, atual))
+if idade == 18:
+    print('Voce tem que se alistar, IMEDIATAMENTE!')
+
+elif idade < 18:
+    saldo = 18 - idade
+    print('Ainda faltam {} anos para o alistamento'.format(saldo))
+    ano = atual + saldo
+elif idade > 18:
+    saldo = idade - 18
+    print('Voce ja deveria ter se alsitado há {} anos'.format(saldo))
+    ano =  atual - saldo
